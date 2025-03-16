4b6edf4c51fdf167440215b3e1f8c25137995dac
Ayesha Safreen S <146083202+Ayeshacodytech@users.noreply.github.com>
2025-03-16 19:54:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,125 +1,113 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
 import { AuthProvider } from "./auth/authContext";
+import store, { persistor } from "./store/store.js"; // Import store and persistor from redux-persist setup
+
+// Import other components and pages
+import "./App.css";
+import AptitudeMCQ from "./components/AptitudeMCQ.jsx";
+import AptitudeMCQs from "./components/aptitudeMCQs.jsx";
 import Article from "./components/article.jsx";
+import TeacherAptitudeMCQs from "./components/teacheraptitudeMCQs.jsx";
+import TeacherAptitudemyMCQs from "./components/teacheraptitudemyMCQs.jsx";
+import TeacherArticle from "./components/teacherarticle.jsx";
+import AddAptitudeMCQ from "./pages/addAptitudemcq.jsx";
+import AddJob from "./pages/addjob.jsx";
+import AdminHome from "./pages/adminHome.jsx";
+import Adminjobdetails from "./pages/adminjobdetails.jsx";
+import { Adminsignin } from "./pages/adminsignin.jsx";
+import Aptitudehome from "./pages/aptitudeHome.jsx";
+import AptitudeProfile from "./pages/aptitudeProfile.jsx";
 import Communityhome from "./pages/CommunityHome.jsx";
 import CommunityProfile from "./pages/communityProfile.jsx";
 import Home from "./pages/home.jsx";
 import JobDetails from "./pages/jobdetails.jsx";
+import { Jobs } from "./pages/jobs.jsx";
 import NewPostForm from "./pages/newPost.jsx";
+import NewResourceForm from "./pages/newResource.jsx";
 import ProfilePage from "./pages/profile.jsx";
 import { Signin } from "./pages/signin.jsx";
 import { Signup } from "./pages/signup.jsx";
-import store from "./store/store.js";
-import { Jobs } from "./pages/jobs.jsx";
-import { Adminsignin } from "./pages/adminsignin.jsx";
-import AdminHome from "./pages/adminHome.jsx";
-import AddJob from "./pages/addjob.jsx";
-import Adminjobdetails from "./pages/adminjobdetails.jsx";
-import { Updatejob } from "./pages/updatejob.jsx";
-import TeacherNewPostForm from "./pages/teachernewPost.jsx";
+import TeacherAptitudehome from "./pages/teacheraptitudeHome.jsx";
+import TeacherAptitudeProfile from "./pages/teacheraptitudeProfile.jsx";
 import TeacherCommunityhome from "./pages/TeacherCommunityHome.jsx";
 import TeacherCommunityProfile from "./pages/teachercommunityProfile.jsx";
 import TeacherHome from "./pages/teacherhome.jsx";
-import TeacherArticle from "./components/teacherarticle.jsx";
-import TeacherSignup from "./pages/teachersignup.jsx";
-import TeacherSignin from "./pages/teachersignin.jsx";
-import Aptitudehome from "./pages/aptitudeHome.jsx";
-import NewResourceForm from "./pages/newResource.jsx";
-import AptitudeMCQs from "./components/aptitudeMCQs.jsx";
-
-import AptitudeProfile from "./pages/aptitudeProfile.jsx";
-import TeacherAptitudehome from "./pages/teacheraptitudeHome.jsx";
-import TeacherAptitudeMCQs from "./components/teacheraptitudeMCQs.jsx";
-import TeacherAptitudemyMCQs from "./components/teacheraptitudemyMCQs.jsx";
-import AptitudeMCQ from "./components/AptitudeMCQ.jsx"
-import TeacherAptitudeProfile from "./pages/teacheraptitudeProfile.jsx";
+import TeacherNewPostForm from "./pages/teachernewPost.jsx";
 import TeacherNewResourceForm from "./pages/teachernewResource.jsx";
-import AddAptitudeMCQ from "./pages/addAptitudemcq.jsx";
+import TeacherSignin from "./pages/teachersignin.jsx";
+import TeacherSignup from "./pages/teachersignup.jsx";
+import { Updatejob } from "./pages/updatejob.jsx";
+import WebSocketListener from "./components/WebSocketListener.jsx";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { setupSocketDebug } from "./utils/SocketDebugUtils.js";
+import UnderDevelopment from "./components/Underdevlopment.jsx";
+ // Import socket helpers
+
 function App() {
+  useEffect(() => {
+    // Wait a bit for socket to be established
+    const timer = setTimeout(() => {
+      if (window._socketInstance) {
+        setupSocketDebug(window._socketInstance);
+      }
+    }, 3000);
+    
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<Navigate to="/signin" replace />} />
-            <Route path="/signin" element={<Signin />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/teacher/home" element={<TeacherHome />}></Route>
-            <Route path="/jobs" element={<Jobs></Jobs>} />
-            <Route path="/jobs/:id" element={<JobDetails />} />
-            <Route path="/admin/jobs/:id" element={<Adminjobdetails />} />
-            <Route path="/admin/update/job/:id" element={<Updatejob />} />
-            <Route path="/profile" element={<ProfilePage></ProfilePage>} />
-            <Route path="/new" element={<NewPostForm></NewPostForm>} />
-            <Route
-              path="/community"
-              element={<Communityhome></Communityhome>}
-            />
-            <Route path="/community/:id" element={<Article></Article>} />
-            <Route
-              path="/communityprofile"
-              element={<CommunityProfile></CommunityProfile>}
-            />
-            <Route path="/admin/signin" element={<Adminsignin />}></Route>
-            <Route path="/teacher/signup" element={<TeacherSignup />}></Route>
-            <Route path="/teacher/signin" element={<TeacherSignin />}></Route>
-            <Route path="/admin/home" element={<AdminHome />}></Route>
-            <Route path="/admin/addjob" element={<AddJob />}></Route>
-            <Route
-              path="/teacher/new"
-              element={<TeacherNewPostForm></TeacherNewPostForm>}
-            />
-            <Route
-              path="/teacher/community"
-              element={<TeacherCommunityhome></TeacherCommunityhome>}
-            />
-            <Route
-              path="/teacher/community/:id"
-              element={<TeacherArticle></TeacherArticle>}
-            />
-            <Route
-              path="/teacher/communityprofile"
-              element={<TeacherCommunityProfile></TeacherCommunityProfile>}
-            />
-            <Route path="/aptitude/mcq" element={<AptitudeMCQs />} />
-            <Route path="/aptitude/mcq/:id" element={<AptitudeMCQ />} />
-            <Route path="/aptitude" element={<Aptitudehome />} />
-            <Route
-              path="/aptitude/new/resource"
-              element={<NewResourceForm />}
-            />
-            <Route
-              path="/teacher/aptitude/new/mcq"
-              element={<AddAptitudeMCQ />}
-            />
-            <Route
-              path="/teacher/aptitude/home"
-              element={<TeacherAptitudehome />}
-            />
-            <Route
-              path="/teacher/aptitude/mcq"
-              element={<TeacherAptitudeMCQs />}
-            />
-            <Route
-              path="/teacher/aptitude/mymcq"
-              element={<TeacherAptitudemyMCQs />}
-            />
-            <Route
-              path="/teacher/aptitude/myresource"
-              element={<TeacherAptitudeProfile />}
-            />
-            <Route
-              path="/teacher/aptitude/new/resource"
-              element={<TeacherNewResourceForm />}
-            />
-            <Route path="/aptitude/myresources" element={<AptitudeProfile />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthProvider>
+          <BrowserRouter>
+          <WebSocketListener />
+
+            {/* Global Toast Container */}
+            <ToastContainer />
+            <Routes>
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/teacher/home" element={<TeacherHome />} />
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/jobs/:id" element={<JobDetails />} />
+              <Route path="/admin/jobs/:id" element={<Adminjobdetails />} />
+              <Route path="/admin/update/job/:id" element={<Updatejob />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/new" element={<NewPostForm />} />
+              <Route path="/community" element={<Communityhome />} />
+              <Route path="/community/:id" element={<Article />} />
+              <Route path="/communityprofile" element={<CommunityProfile />} />
+              <Route path="/admin/signin" element={<Adminsignin />} />
+              <Route path="/teacher/signup" element={<TeacherSignup />} />
+              <Route path="/teacher/signin" element={<TeacherSignin />} />
+              <Route path="/admin/home" element={<AdminHome />} />
+              <Route path="/admin/addjob" element={<AddJob />} />
+              <Route path="/teacher/new" element={<TeacherNewPostForm />} />
+              <Route path="/teacher/community" element={<TeacherCommunityhome />} />
+              <Route path="/teacher/community/:id" element={<TeacherArticle />} />
+              <Route path="/teacher/communityprofile" element={<TeacherCommunityProfile />} />
+              <Route path="/aptitude/mcq" element={<AptitudeMCQs />} />
+              <Route path="/aptitude/mcq/:id" element={<AptitudeMCQ />} />
+              <Route path="/aptitude" element={<Aptitudehome />} />
+              <Route path="/code" element={<UnderDevelopment />} />
+              <Route path="/gate" element={<UnderDevelopment />} />
+              <Route path="/aptitude/new/resource" element={<NewResourceForm />} />
+              <Route path="/teacher/aptitude/new/mcq" element={<AddAptitudeMCQ />} />
+              <Route path="/teacher/aptitude/home" element={<TeacherAptitudehome />} />
+              <Route path="/teacher/aptitude/mcq" element={<TeacherAptitudeMCQs />} />
+              <Route path="/teacher/aptitude/mymcq" element={<TeacherAptitudemyMCQs />} />
+              <Route path="/teacher/aptitude/myresource" element={<TeacherAptitudeProfile />} />
+              <Route path="/teacher/aptitude/new/resource" element={<TeacherNewResourceForm />} />
+              <Route path="/aptitude/myresources" element={<AptitudeProfile />} />
+              <Route path="*" element={<Navigate to="/signin" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
+      </PersistGate>
     </Provider>
   );
 }
