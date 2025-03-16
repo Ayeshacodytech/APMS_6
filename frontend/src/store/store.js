375b0804745800f44f8402771e8eb8d3bc6895a1
Ayesha Safreen S <146083202+Ayeshacodytech@users.noreply.github.com>
2025-03-16 19:54:14 +0530

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,23 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Defaults to localStorage for web
+import { combineReducers } from "redux"; // Required to combine reducers
 import jobsReducer from "./slices/jobSlice";
 import userReducer from "./slices/userSlice";
 import communityReducer from "./slices/communitySlice";
 import teachercommunityReducer from "./slices/teachercommunityslice";
-import notificationReducer from "./slices/notificationSlice"
-import teachernotificationReducer from "./slices/teachernotificationSlice"
-import teacheraptitudeReducer from "./slices/teacherAptitudeslice"
+import notificationReducer from "./slices/notificationSlice";
+import teachernotificationReducer from "./slices/teachernotificationSlice";
+import teacheraptitudeReducer from "./slices/teacherAptitudeslice";
 import aptitudeReducer from "./slices/aptitudeSlice";
-const store = configureStore({
-    reducer: {
-        jobs: jobsReducer,
-        user:userReducer,
-        community:communityReducer,
-        notifications:notificationReducer,
-        teachercommunity:teachercommunityReducer,
-        teachernotifications:teachernotificationReducer,
-        teacheraptitude:teacheraptitudeReducer,
-        aptitude:aptitudeReducer
-    },
+import socketReducer from "./slices/socketSlice";
+// Redux persist configuration
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+// Combine all slice reducers into a single root reducer function
+const rootReducer = combineReducers({
+    jobs: jobsReducer,
+    user: userReducer,
+    community: communityReducer,
+    notifications: notificationReducer,
+    teachercommunity: teachercommunityReducer,
+    teachernotifications: teachernotificationReducer,
+    teacheraptitude: teacheraptitudeReducer,
+    aptitude: aptitudeReducer,
+    socket: socketReducer,
 });
+
+// Wrap the rootReducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure and create the Redux store
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+// Create the persistor instance
+export const persistor = persistStore(store);
 
 export default store;
