7d6073bf0a4bb75b0d7d87ac3f382a0df138a4d2
Ayesha Safreen S <146083202+Ayeshacodytech@users.noreply.github.com>
2025-03-16 20:18:12 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ let io;
 const init = (server) => {
     io = new Server(server, {
         cors: {
-            origin: '*',
+            origin: 'https://futureforge-nine.vercel.app',
             methods: ['GET', 'POST'],
         }
     });
