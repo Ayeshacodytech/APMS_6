38883b9e11cde11d344614a7469daa05dc610d1c
Ayesha Safreen S <146083202+Ayeshacodytech@users.noreply.github.com>
2025-04-04 00:33:17 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,8 @@ const teacherRouter=require("./teacher");
 const teachercommunityRouter=require("./teachercommunity");
 const aptitudeRouter=require("./aptitude");
 const gateRouter=require("./gate");
+const submissionRouter=require("./submission");
+const problemRouter=require("./problem");
 const router=express.Router();
 
 router.use("/student",studentRouter);
@@ -17,4 +19,7 @@ router.use('/gate',gateRouter)
 router.use('/teacher',teacherRouter)
 router.use('/notification',notificationRouter)
 router.use('/teacher/community',teachercommunityRouter)
+router.use('/submission',submissionRouter)
+router.use('/problem',problemRouter)
+
 module.exports = router;
